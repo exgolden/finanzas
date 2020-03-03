cb6004ef7907b271f7fd154ba185e5034e3f7ca7
exgolden <59813207+exgolden@users.noreply.github.com>
2020-03-03 01:32:49 -0600

    Update finance.py

--- finance.py
+++ finance.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 #Request
-browser=webdriver.Firefox(executable_path="C:\BrowserDriver\geckodriver.exe")
+browser=webdriver.Firefox(executable_path=r"C:\BrowserDriver\geckodriver.exe")
 browser.get("https://finance.yahoo.com/")
 searchbar=browser.find_element_by_id("yfin-usr-qry")
 searchbar.send_keys("TSLA")
